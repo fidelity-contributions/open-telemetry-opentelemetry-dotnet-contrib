d71991406e44293cb8c722ec3237590b6e6e8d8a
Martin Costello <martin@martincostello.com>
2026-04-29 17:33:11 +0100

    [SqlClient] Use SearchValues more in SqlProcessor (#4303)

--- test/OpenTelemetry.Contrib.Shared.Tests/SqlProcessorTests.cs
+++ test/OpenTelemetry.Contrib.Shared.Tests/SqlProcessorTests.cs
@@ -29,6 +29,17 @@ public class SqlProcessorTests
     }
 
     [Fact]
+    public void GetSanitizedSql_SingleLineCommentWithCarriageReturnLineFeed_PreservesLineBreak()
+    {
+        var sql = "SELECT * FROM table -- comment\r\nWHERE id = 42";
+
+        var sqlStatementInfo = SqlProcessor.GetSanitizedSql(sql);
+
+        Assert.Equal("SELECT * FROM table \r\nWHERE id = ?", sqlStatementInfo.SanitizedSql);
+        Assert.Equal("SELECT table", sqlStatementInfo.DbQuerySummary);
+    }
+
+    [Fact]
     public void GetSanitizedSql_UnterminatedEscapedIdentifierInFromClause_SanitizesLiterals()
     {
         var sql = "SELECT * FROM [Orders WHERE CustomerName = 'secret-name' AND Id = 123 AND Token = 0xDEADBEEF";
